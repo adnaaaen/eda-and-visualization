61226fc3dcda72275061bc19faa32d8b0de1ff58
Adnan <adnanmuhammedadnancp@gmail.com>
2024-10-18 00:29:10 +0530

    chore(script) : add script files

---
+++ EV-vechicle/data/README.md
@@ -0,0 +1,8 @@
+
+# About Dataset 📊
+
+This dataset shows the Battery Electric Vehicles (BEVs) and Plug-in Hybrid Electric Vehicles (PHEVs) that are currently registered through Washington State Department of Licensing (DOL).
+
+## [Link to Dataset]("https://catalog.data.gov/dataset/electric-vehicle-population-data")
+
+
--- EV-vechicle/scripts/visualizing.py
+++ EV-vechicle/scripts/visualizing.py
@@ -1,15 +1,21 @@
 from matplotlib import pyplot as plt
-from seaborn import countplot
+from seaborn import histplot
 from wordcloud import WordCloud
 
 # types
-# from typing import Any
-# from matplotlib.axis import Axis
-# from matplotlib.figure import Figure
 from pandas import Series
 
 
 def plot_wordcloud(data: Series) -> None:
+    """
+    return wordcloud of its values
+    Args:
+        data (Series) : data to be create wordcloud
+
+    Return:
+        None
+    """
+
     word_cloud = WordCloud(width=800, height=600).generate_from_frequencies(data.value_counts())
     plt.title(data.name, fontsize=28)
     plt.imshow(word_cloud)
@@ -19,4 +25,44 @@ def plot_wordcloud(data: Series) -> None:
 # ---------------------------------------------------------------------------------
 
 
-# TODO: catagorical distribution
+def plot_catagorical(data: Series) -> None:
+
+    """
+    plot catagorical about the top 10 value counts in the data
+    Args:
+        data (Series) : data to be create wordcloud
+
+    Return:
+        None
+    """
+    
+    highest_data = data.value_counts().nlargest(n=10)
+    plt.title(f"TOP 10 {data.name}", fontsize=24)
+    plt.bar(x=highest_data.index, height=highest_data.values)
+    plt.xticks(rotation=90)
+    plt.show()
+
+
+# ---------------------------------------------------------------------------------
+
+
+def plot_numerical(data: Series) -> None:
+    """
+"
+    plot numerical variable with histogram to find the distribution
+    Args:
+        data (Series) : data to be create wordcloud
+
+    Return:
+        None
+    "
+    """
+
+    plt.title(f"DISTRIBUTION OF {data.name}", fontsize=20)
+    histplot(data, bins=20, kde=True)
+    plt.show()
+
+
+
+
+    
